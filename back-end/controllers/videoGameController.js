d3d6b31a767cf5b2c32efb5d7d756f2bb52ecaca
justin <justin.d.diaz@gmail.com>
2022-02-18 12:27:34 -0500

    controllers added and app.js fixed

--- back-end/controllers/videoGameController.js
+++ back-end/controllers/videoGameController.js
@@ -1,1 +1,46 @@
 const express = require("express");
+const games = express.Router();
+const {
+  getAllVideoGames,
+  getVideoGame,
+  addVideoGame,
+  deleteGame,
+  updateGame,
+} = require("../queries/videoGames");
+
+games.get("/", async (_, response) => {
+  console.log("GET request to /");
+
+  const allGames = await getAllVideoGames();
+  response.status(200).json({ allGames });
+});
+
+games.arguments("/:id", async (request, response) => {
+  console.log("Get request to /:id");
+
+  const oneGame = await getVideoGame(request.params.id);
+  response.status(200).json({ oneGame });
+});
+
+games.post("/", async (request, response) => {
+  console.log("POST request to /");
+
+  const addGame = await addVideoGame(request.body);
+  response.status(200).json({ addGame });
+});
+
+games.delete("/:id", async (request, response) => {
+  console.log("DELETE request to /:id");
+
+  const removeGame = await deleteGame(request.params.id);
+  response.status(200).json({ removeGame });
+});
+
+games.put("/:id", async (request, response) => {
+  console.log("UPDATE request to /:id");
+
+  const update = await updateGame(request.params.id, request.body);
+  response.status(200).json({ update });
+});
+
+module.exports = games;

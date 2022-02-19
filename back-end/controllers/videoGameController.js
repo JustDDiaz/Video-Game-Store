c815da07380dabf140f4f1afd1cebcde94b8dc02
justin <justin.d.diaz@gmail.com>
2022-02-18 21:58:49 -0500

    fix

--- back-end/controllers/videoGameController.js
+++ back-end/controllers/videoGameController.js
@@ -12,35 +12,35 @@ games.get("/", async (_, response) => {
   console.log("GET request to /");
 
   const allGames = await getAllVideoGames();
-  response.status(200).json({ allGames });
+  response.status(200).json(allGames);
 });
 
 games.get("/:id", async (request, response) => {
-  console.log("Get request to /:id");
+  console.log("GET request to /:id");
 
   const oneGame = await getVideoGame(request.params.id);
-  response.status(200).json({ oneGame });
+  response.status(200).json(oneGame);
 });
 
 games.post("/", async (request, response) => {
   console.log("POST request to /");
 
   const addGame = await addVideoGame(request.body);
-  response.status(200).json({ addGame });
+  response.status(200).json(addGame);
 });
 
 games.delete("/:id", async (request, response) => {
   console.log("DELETE request to /:id");
 
   const removeGame = await deleteGame(request.params.id);
-  response.status(200).json({ removeGame });
+  response.status(200).json(removeGame);
 });
 
 games.put("/:id", async (request, response) => {
   console.log("UPDATE request to /:id");
 
   const update = await updateGame(request.params.id, request.body);
-  response.status(200).json({ update });
+  response.status(200).json(update);
 });
 
 module.exports = games;
--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -5,7 +5,14 @@ CREATE DATABASE video_game_store;
 
 DROP TABLE IF EXISTS games;
 
-CREATE TABLE games (id SERIAL PRIMARY KEY, name TEXT NOT NULL, description TEXT, price INT, rating INT DEFAULT 5, CHECK (rating >= 0 AND rating <= 5), featured BOOLEAN, image TEXT
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    price INT, 
+    rating INT DEFAULT 5, CHECK (rating >= 0 AND rating <= 5), 
+    featured BOOLEAN, 
+    image TEXT
 );
 
 
